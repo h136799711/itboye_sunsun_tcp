5133b0b7eee197c86a133df9d9b71be8e2a9d779
hebidu <hebiduhebi@126.com>
2017-03-31 12:31:23 +0800

    【修改】 des采用openssl

--- sunsun/src/helper/Des.php
+++ sunsun/src/helper/Des.php
@@ -18,58 +18,29 @@ namespace sunsun\helper;
 class Des{
 
 
-
     /**
      *加密
-     * @param <type> $value
-     * @return <type>
+     * @param $value
+     * @param $key
+     * @return string <type>
+     * @internal param $ <type> $value
      */
     public static function encrypt ($value,$key)
     {
-        $td = mcrypt_module_open(MCRYPT_DES, '', MCRYPT_MODE_ECB, '');
-        $iv = substr($key,0,8);
-        mcrypt_generic_init($td, $key, $iv);
-        $value = self::PaddingPKCS7($value);
-        $ret = base64_encode(mcrypt_generic($td, $value));
-        mcrypt_generic_deinit($td);
-        mcrypt_module_close($td);
+        $ret = base64_encode(openssl_encrypt($value,"des-ecb",$key));
         return $ret;
     }
 
     /**
      *解密
-     * @param <type> $value
-     * @return <type>
+     * @param $value
+     * @param $key
+     * @return string <type>
+     * @internal param $ <type> $value
      */
     public static function decrypt ($value,$key)
     {
-        $td = mcrypt_module_open(MCRYPT_DES, '', MCRYPT_MODE_ECB, '');
-        $iv = substr($key,0,8);
-        mcrypt_generic_init($td, $key, $iv);
-        $ret = trim(mdecrypt_generic($td, base64_decode($value)));
-        $ret = self::UnPaddingPKCS7($ret);
-        mcrypt_generic_deinit($td);
-        mcrypt_module_close($td);
-        return $ret;
+        return openssl_decrypt(base64_decode($value),"des-ecb",$key);
     }
 
-    private static function PaddingPKCS7 ($data)
-    {
-        $block_size = mcrypt_get_block_size(MCRYPT_DES, 'ecb');
-        $padding_char = $block_size - (strlen($data) % $block_size);
-        $data .= str_repeat(chr($padding_char), $padding_char);
-        return $data;
-    }
-
-    private static function UnPaddingPKCS7($text)
-    {
-        $pad = ord($text{strlen($text) - 1});
-        if ($pad > strlen($text)) {
-            return false;
-        }
-        if (strspn($text, chr($pad), strlen($text) - $pad) != $pad) {
-            return false;
-        }
-        return substr($text, 0, - 1 * $pad);
-    }
 }
