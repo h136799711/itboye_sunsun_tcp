2cb5a08b104c4c32d63bb9e906c234f33d9ea62c
hebidu <hebiduhebi@126.com>
2017-04-01 10:15:49 +0800

    【完善】shell多条件 非 -o

--- git_update.sh
+++ git_update.sh
@@ -1,3 +1,3 @@
 #git 更新test456789
 #pull的情况是正式环境下用pull，测试环境用fetch
-git fetch origin master | get reset --hard master | composer update
+git fetch origin master | get reset --hard origin/master | composer update
