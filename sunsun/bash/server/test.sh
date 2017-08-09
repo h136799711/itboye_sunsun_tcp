5ce94b9baf0def9204346ee0752831b05476de5a
hebidu <hebiduhebi@126.com>
2017-08-09 11:54:55 +0800

    【增加】 一个模拟tcp server，不做任何操作，只接受链接

--- sunsun/bash/server/test.sh
+++ sunsun/bash/server/test.sh
@@ -1,1 +1,1 @@
- php ../../src/server/start_server_register.php start  && php ../../src/server/start_gateway.php start  && php ../../src/server/start_businessworker.php start
+ php ../../src/server/start_server_register.php start -d  && php ../../src/server/start_gateway.php start -d && php ../../src/server/start_businessworker.php start
