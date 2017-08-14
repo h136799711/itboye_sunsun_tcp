a3c66c1402415bd1380cbee3292ac570479453a7
hebidu <hebiduhebi@126.com>
2017-08-14 13:47:50 +0800

    【完善】增加变频水泵gateway

--- sunsun/bash/server/status.sh
+++ sunsun/bash/server/status.sh
@@ -1,3 +1,4 @@
 php ../../src/server/start_gateway.php status
+php ../../src/server/water_pump_gateway_8286.php status
 php ../../src/server/start_businessworker.php status
 php ../../src/server/start_sserver_register.php status
