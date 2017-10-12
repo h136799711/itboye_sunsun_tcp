6c39a5938b80f15ce7d244255f0680bf9cb9525d
hebidu <hebiduhebi@126.com>
2017-10-12 15:40:33 +0800

    【增加】暂时暂停

--- sunsun/src/server/business/DebugHelper.php
+++ sunsun/src/server/business/DebugHelper.php
@@ -9,8 +9,6 @@
 namespace sunsun\server\business;
 
 
-use sunsun\transfer_station\client\TransferClient;
-
 class DebugHelper
 {
     const HEBIDU_GROUP = 'hebidu';
@@ -54,10 +52,10 @@ class DebugHelper
     }
 
     public static function sendToHebidu($message){
-        try{
-            TransferClient::sendMessageToGroup(self::HEBIDU_GROUP, $message,date('Y-m-d H:i:s ',time()));
-        }catch (\Exception $ex){
-            //不处理
-        }
+//        try{
+//            TransferClient::sendMessageToGroup(self::HEBIDU_GROUP, $message,date('Y-m-d H:i:s ',time()));
+//        }catch (\Exception $ex){
+//            //不处理
+//        }
     }
 }
