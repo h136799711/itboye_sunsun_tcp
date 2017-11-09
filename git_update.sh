69d349b8d2854651171800cebc7a253fa954232e
hebidu <hebiduhebi@126.com>
2017-11-09 14:29:55 +0800

    【修复】过滤桶设置修复range字段

--- git_update.sh
+++ git_update.sh
@@ -1,2 +1,2 @@
 #pull的情况是正式环境下用pull，测试环境用fetch
-git fetch origin master && git reset --hard origin/master && composer install
+git fetch origin master && git reset --hard origin/master && composer dump-autoload --optimize
--- git_update_dev.sh
+++ git_update_dev.sh
@@ -1,2 +1,2 @@
 #pull的情况是正式环境下用pull，测试环境用fetch
-git fetch origin dev && git reset --hard origin/dev && composer install
+git fetch origin dev && git reset --hard origin/dev  && composer dump-autoload --optimize
