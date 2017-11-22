ed26eaae3b3d89116a1dffc60f5abf7f1e928632
hebidu <hebiduhebi@126.com>
2017-11-22 19:40:32 +0800

    [增加] DebugEvents

--- sunsun/src/server/business/DebugEvents.php
+++ sunsun/src/server/business/DebugEvents.php
@@ -41,7 +41,7 @@ use Workerman\Worker;
  * 主要是处理 onConnect onMessage onClose 三个方法
  * onConnect 和 onClose 如果不需要可以不用实现并删除
  */
-class Events
+class DebugEvents
 {
     /**
      * @var  \sunsun\server\db\DbPool
