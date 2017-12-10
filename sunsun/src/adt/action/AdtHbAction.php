6ea9c3c8c72816f37a8004f2cbff520258cdb4ab
hebidu <hebiduhebi@126.com>
2017-12-10 11:57:45 +0800

    [增加] 增加adt在线时间统计

--- sunsun/src/adt/action/AdtHbAction.php
+++ sunsun/src/adt/action/AdtHbAction.php
@@ -9,7 +9,10 @@
 namespace sunsun\adt\action;
 
 
+use sunsun\server\factory\DeviceFacadeFactory;
+use sunsun\server\factory\RespFacadeFactory;
 use sunsun\server\interfaces\BaseAction;
+use sunsun\server\req\BaseHeartBeatClientReq;
 
 /**
  * Class AdtHbAction
@@ -18,4 +21,18 @@ use sunsun\server\interfaces\BaseAction;
  */
 class AdtHbAction extends BaseAction
 {
+
+    /**
+     * 通用设备心跳请求
+     * @param $did
+     * @param $clientId
+     * @param BaseHeartBeatClientReq $req
+     * @return null|\sunsun\adt\resp\AdtCtrlDeviceResp|\sunsun\adt\resp\AdtDeviceInfoResp|\sunsun\adt\resp\AdtDeviceUpdateResp|\sunsun\aph300\resp\Aph300CtrlDeviceResp|\sunsun\aph300\resp\Aph300DeviceInfoResp|\sunsun\aph300\resp\Aph300DeviceUpdateResp|\sunsun\aq806\resp\Aq806CtrlDeviceResp|\sunsun\aq806\resp\Aq806DeviceInfoResp|\sunsun\aq806\resp\Aq806DeviceUpdateResp|\sunsun\cp1000\resp\Cp1000CtrlDeviceResp|\sunsun\cp1000\resp\Cp1000DeviceFirmwareUpdateResp|\sunsun\cp1000\resp\Cp1000DeviceInfoResp|\sunsun\cp1000\resp\Cp1000HbResp|\sunsun\heating_rod\resp\HeatingRodCtrlDeviceResp|\sunsun\heating_rod\resp\HeatingRodDeviceInfoResp|\sunsun\heating_rod\resp\HeatingRodDeviceUpdateResp
+     */
+    public function deviceHeartBeat($did, $clientId, BaseHeartBeatClientReq $req)
+    {
+        (DeviceFacadeFactory::getDeviceDal($did))->updateByDidOnline($did);
+        $respObj = RespFacadeFactory::createHeartBeatRespObj($did, $req);
+        return $respObj;
+    }
 }
