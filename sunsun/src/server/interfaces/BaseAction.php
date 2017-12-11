9ee55ac28ec259c06376c78549496138eff66b17
hebidu <hebiduhebi@126.com>
2017-12-11 18:00:36 +0800

    【更新】心跳改版

--- sunsun/src/server/interfaces/BaseAction.php
+++ sunsun/src/server/interfaces/BaseAction.php
@@ -32,6 +32,16 @@ use sunsun\transfer_station\controller\RespMsgType;
  */
 abstract class BaseAction
 {
+
+    /**
+     * 心跳计数最大 - 例如等于2 则每2次心跳才更新一次数据库
+     * 0 < 心跳次数  < 720 / 心跳间隔时间（默认是120） 这个是离线检测的时间
+     * 查看
+     * itboye_sunsunxiaoli项目的
+     * app\index\helper\CommandHelper::offline_notify 中的定义
+     */
+    const HB_COUNT_MAX = 2;
+
     /**
      * 最近插入数据库的缓存数据
      */
@@ -75,7 +85,21 @@ abstract class BaseAction
      */
     public function deviceHeartBeat($did, $clientId, BaseHeartBeatClientReq $req)
     {
-        (DeviceFacadeFactory::getDeviceDal($did))->updateByDid($did, ['update_time' => time()]);
+        $session = Gateway::getSession($clientId);
+        $hbCnt = 1;
+
+        if ($session && array_key_exists('hb_cnt', $session)) {
+            $hbCnt = intval($session['hb_cnt']);
+            if ($hbCnt < self::HB_COUNT_MAX) {
+                $hbCnt++;
+            } else {
+                $hbCnt = 1;
+                (DeviceFacadeFactory::getDeviceDal($did))->updateByDid($did, ['update_time' => time()]);
+            }
+        }
+
+        Gateway::updateSession($clientId, ['hb_cnt' => $hbCnt]);
+
         $respObj = RespFacadeFactory::createHeartBeatRespObj($did, $req);
         return $respObj;
     }
