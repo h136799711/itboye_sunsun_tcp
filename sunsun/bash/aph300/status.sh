7619343eab3bf5129b8b03de486ab5e09b73aa69
hebidu <hebiduhebi@126.com>
2018-05-18 14:30:32 +0800

    [aq806] 分开定时检测的worker

--- sunsun/bash/aph300/status.sh
+++ sunsun/bash/aph300/status.sh
@@ -1,3 +1,4 @@
 php ../../src/aph300/start_gateway.php status
 php ../../src/aph300/start_businessworker.php status
 php ../../src/aph300/start_aph300_register.php status
+php ../../src/aph300/timer_worker.php status
