7ed2ad5cb1578cdd4add4feed3eb82858febc2de
hebidu <hebiduhebi@126.com>
2018-05-18 14:28:59 +0800

    [aq806] 分开定时检测的worker

--- sunsun/bash/aph300/reload.sh
+++ sunsun/bash/aph300/reload.sh
@@ -1,3 +1,4 @@
 php ../../src/aph300/start_gateway.php reload
 php ../../src/aph300/start_businessworker.php reload
 php ../../src/aph300/start_aph300_register.php reload
+php ../../src/aph300/timer_worker.php reload
--- sunsun/bash/aph300/restart.sh
+++ sunsun/bash/aph300/restart.sh
@@ -1,3 +1,4 @@
 php ../../src/aph300/start_gateway.php restart
 php ../../src/aph300/start_businessworker.php restart
 php ../../src/aph300/start_aph300_register.php restart
+php ../../src/aph300/timer_worker.php restart
