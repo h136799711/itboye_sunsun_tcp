bbcd99e5f5e8bb545b3fae9f4114c4b5da7d2088
hebidu <hebiduhebi@126.com>
2018-05-18 14:18:34 +0800

    [aq806] 分开定时检测的worker

--- sunsun/bash/aq806/status.sh
+++ sunsun/bash/aq806/status.sh
@@ -1,3 +1,4 @@
 php ../../src/aq806/start_gateway.php status
 php ../../src/aq806/start_businessworker.php status
 php ../../src/aq806/start_aq806_register.php status
+php ../../src/aq806/timer_worker.php status
