736ab081fabe462f2081aeb4b4d861fdab1a22aa
hebidu <hebiduhebi@126.com>
2018-05-21 15:13:48 +0800

    [aq806] 优化定时检测的worker

--- sunsun/bash/water_pump/status.sh
+++ sunsun/bash/water_pump/status.sh
@@ -1,3 +1,4 @@
 php ../../src/water_pump/start_gateway.php status
 php ../../src/water_pump/start_businessworker.php status
 php ../../src/water_pump/start_water_pump_register.php status
+php ../../src/water_pump/timer_worker.php status
