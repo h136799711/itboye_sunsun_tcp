956a3e1bc132a18cb79b5485fb333a12418ed12c
hebidu <hebiduhebi@126.com>
2018-05-28 17:34:50 +0800

    [更新] 更换数据库

--- sunsun/src/server/db/DbPool.php
+++ sunsun/src/server/db/DbPool.php
@@ -12,7 +12,9 @@ namespace sunsun\server\db;
 use sunsun\server\consts\DeviceType;
 use Workerman\MySQL\Connection;
 
-define("SUNSUN_WORKER_HOST", "db1.sunsunxiaoli.com");
+#define("SUNSUN_WORKER_HOST", "db1.sunsunxiaoli.com");
+// 外网rds
+define("SUNSUN_WORKER_HOST", "rm-bp1utkoj82w47hlamxo.mysql.rds.aliyuncs.com");
 define("SUNSUN_WORKER_PORT", "3306");
 define("SUNSUN_WORKER_USER", "sunsun");
 define("SUNSUN_WORKER_PASSWORD", "poiuyTREWQ123456");
