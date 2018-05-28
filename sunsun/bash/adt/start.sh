04d8750df1f5a853536f6d01313f11d18c5d1c30
hebidu <hebiduhebi@126.com>
2018-05-28 16:44:31 +0800

    【adt】 adt设备完善

--- sunsun/bash/adt/start.sh
+++ sunsun/bash/adt/start.sh
@@ -1,1 +1,1 @@
- php ../../src/adt/start_adt_register.php start -d && php ../../src/adt/start_gateway.php start -d && php ../../src/adt/start_businessworker.php start -d
+php ../../src/adt/timer_worker.php stop &&  php ../../src/adt/start_adt_register.php start -d && php ../../src/adt/start_gateway.php start -d && php ../../src/adt/start_businessworker.php start -d
