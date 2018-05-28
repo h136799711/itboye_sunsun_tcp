630fbe26433201fea4c40a798e148f371254bea5
hebidu <hebiduhebi@126.com>
2018-05-28 17:06:20 +0800

    【cp1000】 cp1000

--- sunsun/bash/cp1000/status.sh
+++ sunsun/bash/cp1000/status.sh
@@ -1,3 +1,4 @@
 php ../../src/cp1000/start_gateway.php status
 php ../../src/cp1000/start_businessworker.php status
 php ../../src/cp1000/start_cp1000_register.php status
+php ../../src/aq806/timer_worker.php status
