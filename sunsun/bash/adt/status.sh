950d9d935822cc1b05d0bc5c04beb7f3a1d73ca4
hebidu <hebiduhebi@126.com>
2018-05-28 16:46:55 +0800

    【adt】 adt设备完善

--- sunsun/bash/adt/status.sh
+++ sunsun/bash/adt/status.sh
@@ -1,3 +1,4 @@
 php ../../src/adt/start_gateway.php status
 php ../../src/adt/start_businessworker.php status
 php ../../src/adt/start_adt_register.php status
+php ../../src/adt/timer_worker.php status
