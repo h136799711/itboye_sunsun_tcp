ca579be3d5f9969c1a600f1a8bb414ef5b108d16
hebidu <hebiduhebi@126.com>
2018-06-23 11:36:52 +0800

    [更新] 更新aq806,water_pump ,aph300获取设备信息模式

--- sunsun/bash/aph300/restart.sh
+++ sunsun/bash/aph300/restart.sh
@@ -1,4 +1,3 @@
 php ../../src/aph300/start_gateway.php restart
 php ../../src/aph300/start_businessworker.php restart
 php ../../src/aph300/start_aph300_register.php restart
-php ../../src/aph300/timer_worker.php restart
--- sunsun/bash/aph300/start.sh
+++ sunsun/bash/aph300/start.sh
@@ -1,1 +1,1 @@
-php ../../src/aph300/timer_worker.php start -d &&  php ../../src/aph300/start_aph300_register.php start -d && php ../../src/aph300/start_gateway.php start -d && php ../../src/aph300/start_businessworker.php start -d
+php ../../src/aph300/start_aph300_register.php start -d && php ../../src/aph300/start_gateway.php start -d && php ../../src/aph300/start_businessworker.php start -d
