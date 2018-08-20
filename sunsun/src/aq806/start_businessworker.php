86d3b34f29e5c3c6c2dc849c3254189ef3e17805
hebidu <hebiduhebi@126.com>
2018-08-20 18:29:50 +0800

    [更新] Events.php

--- sunsun/src/aq806/start_businessworker.php
+++ sunsun/src/aq806/start_businessworker.php
@@ -26,7 +26,7 @@ $worker->name = 'aq806_worker';
 // bussinessWorker进程数量
 $worker->count = 10;
 // 设置业务处理类
-$worker->eventHandler = "\sunsun\server\business\Events";
+$worker->eventHandler = "\sunsun\server\business\SlaveEvents";
 // 服务注册地址
 $worker->registerAddress = '127.0.0.1:1238';
 // 进程启动时设置一个定时器，定时向所有客户端连接发送数据
