722a94cfd46dcfb8391ef92ebde8a6a749b48a35
hebidu <hebiduhebi@126.com>
2018-08-20 19:07:43 +0800

    [更新] Events.php

--- sunsun/src/server/interfaces/BaseAction.php
+++ sunsun/src/server/interfaces/BaseAction.php
@@ -308,7 +308,7 @@ abstract class BaseAction
             if (SlaveEvents::$mqttClient != null) {
                 SlaveEvents::$mqttClient->publish("event_".substr($did, 0, 3), json_encode($data),
                 ['qos' => 0, 'retain' => false, 'dup' => false], function(\Exception $exception) {
-                    SlaveEvents::sendEmailTo($exception->getMessage(), "aq806内部发送事件异常");
+                    //SlaveEvents::sendEmailTo($exception->getMessage(), "aq806内部发送事件异常");
                 });
             }
         } catch (\Exception $exception) {
