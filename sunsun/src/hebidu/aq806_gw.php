a399706dd15b1834e8b5d7be681541733aefa6c7
hebidu <hebiduhebi@126.com>
2018-08-24 15:13:39 +0800

    [更新] 日志记录到文件

--- sunsun/src/hebidu/aq806_gw.php
+++ sunsun/src/hebidu/aq806_gw.php
@@ -30,7 +30,7 @@ $gateway->lanIp =  '172.24.72.11';
 // 则一般会使用 4个端口作为内部通讯端口
 $gateway->startPort = 3420;
 // 服务注册地址
-$gateway->registerAddress = '127.0.0.1:1238';
+$gateway->registerAddress = 'master.sunsunxiaoli.com:1238';
 
 // 心跳间隔
 $gateway->pingInterval = 360;
