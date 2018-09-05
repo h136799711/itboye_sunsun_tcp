8ccfb14242e53f45f4a2f2b164a6733a1be39017
hebidu <hebiduhebi@126.com>
2018-09-05 15:52:16 +0800

    增加 PetFeeder 设备

--- sunsun/src/ServerAddress.php
+++ sunsun/src/ServerAddress.php
@@ -15,4 +15,15 @@ class ServerAddress
     const SLAVE_01_INNER_IP = "172.16.23.85";
     const REGISTER_IP = "172.16.23.84";
 
+    const Filter_vat_Register = "172.16.23.84:1237";
+    const Aq806_Register = "172.16.23.84:1238";
+    const Heating_rod_Register = "172.16.23.84:1239";
+    const Aph300_Register = "172.16.23.84:1240";
+    const Water_pump_Register = "172.16.23.84:1241";
+    const Adt_Register = "172.16.23.84:1242";
+    const Cp1000_Register = "172.16.23.84:1243";
+    const Aq118_Register = "172.16.23.84:1244";
+    const Feeder_Register = "172.16.23.84:1245";
+    const Pet_Feeder_Register = "172.16.23.84:1246";
+    
 }
