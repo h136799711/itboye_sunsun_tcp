366c3ab7d05200de9bc20f71a1ded71d53e79943
hebidu <hebiduhebi@126.com>
2018-10-22 10:58:17 +0800

    增加 PetFeeder 设备

--- sunsun/src/server/req/BaseDeviceEventClientReq.php
+++ sunsun/src/server/req/BaseDeviceEventClientReq.php
@@ -24,6 +24,18 @@ abstract class BaseDeviceEventClientReq extends BaseReqPo
         if (!empty($data) && array_key_exists('code', $data)) {
             $this->setCode($data['code']);
         }
+        if (!empty($data) && array_key_exists('m', $data)) {
+            $this->setM($data['m']);
+        }
+        if (!empty($data) && array_key_exists('cam_did', $data)) {
+            $this->setCamDid($data['cam_did']);
+        }
+        if (!empty($data) && array_key_exists('cam_pwd', $data)) {
+            $this->setCamPwd($data['cam_pwd']);
+        }
+        if (!empty($data) && array_key_exists('file', $data)) {
+            $this->setFile($data['file']);
+        }
     }
 
     function toDataArray()
@@ -31,11 +43,84 @@ abstract class BaseDeviceEventClientReq extends BaseReqPo
         return [
             'reqType' => $this->getReqType(),
             'sn' => $this->getSn(),
-            'code' => $this->getCode()
+            'code' => $this->getCode(),
+            'm' => $this->getM(),
+            'file' => $this->getFile(),
+            'cam_did' => $this->getCamDid(),
+            'cam_pwd' => $this->getCamPwd()
         ];
     }
 
+    private $file;
+    private $camDid;
+    private $camPwd;
+    private $m;
+
     private $code;
+
+    /**
+     * @return mixed
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @param mixed $file
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCamDid()
+    {
+        return $this->camDid;
+    }
+
+    /**
+     * @param mixed $camDid
+     */
+    public function setCamDid($camDid)
+    {
+        $this->camDid = $camDid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCamPwd()
+    {
+        return $this->camPwd;
+    }
+
+    /**
+     * @param mixed $camPwd
+     */
+    public function setCamPwd($camPwd)
+    {
+        $this->camPwd = $camPwd;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getM()
+    {
+        return $this->m;
+    }
+
+    /**
+     * @param mixed $m
+     */
+    public function setM($m)
+    {
+        $this->m = $m;
+    }
 
     /**
      * @return mixed
@@ -52,4 +137,5 @@ abstract class BaseDeviceEventClientReq extends BaseReqPo
     {
         $this->code = $code;
     }
+
 }
